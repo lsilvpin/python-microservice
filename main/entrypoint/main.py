6f3d01c182e5b93b1b00aea4ec81c7e371a069cb
lsilvpin <lsilvpin@gmail.com>
2024-03-31 20:16:36 -0300

    [Jenkins] Melhoria no microserviço

--- main/entrypoint/main.py
+++ main/entrypoint/main.py
@@ -5,10 +5,18 @@ from main.library.utils.core.settings_helper import load_environment, get
 import uvicorn
 from main.library.di_container import Container
 from main.entrypoint.controllers.main_controller import router as main_controller_router
+from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
 
 load_environment(get("environment"))
 container = Container()
-app = FastAPI()
+app = FastAPI(
+    title="My API",
+    description="This is a sample API",
+    version="1.0.0",
+    docs_url="/docs",
+    redoc_url="/redoc"
+)
 app.container = container
 app.include_router(main_controller_router)
 
