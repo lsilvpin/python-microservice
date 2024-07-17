0559fbc7bdc9cfbcfaac655625e191de2fcd5735
lsilvpin <lsilvpin@gmail.com>
2024-07-16 22:41:30 -0300

    Ajuste no health check

--- test-is-up.sh
+++ test-is-up.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-host_name=${1:-"localhost"}
-port=${2:-"8000"}
+host_name=${1:-"wsl"}
+port=${2:-"32003"}
 protocol=${3:-"http"}
 
 echo "Checking if service is up..."
 
 limit_counter=0
-
 while [ $(curl -s -o /dev/null -w "%{http_code}" $protocol://$host_name:$port/info) != "200" ]; do
-
     echo "Service is not up yet. Waiting for 5 seconds..."
     sleep 5
     limit_counter=$((limit_counter+1))
