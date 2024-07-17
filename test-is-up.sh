daddd61cddb66a1a0b42b72c2f44c8050242aa25
lsilvpin <lsilvpin@gmail.com>
2024-07-17 09:28:45 -0300

    Teste deve bater em /health

--- test-is-up.sh
+++ test-is-up.sh
@@ -7,7 +7,7 @@ protocol=${3:-"http"}
 echo "Checking if service is up..."
 
 limit_counter=0
-while [ $(curl -s -o /dev/null -w "%{http_code}" $protocol://$host_name:$port/info) != "200" ]; do
+while [ $(curl -s -o /dev/null -w "%{http_code}" $protocol://$host_name:$port/health) != "200" ]; do
     echo "Service is not up yet. Waiting for 5 seconds..."
     sleep 5
     limit_counter=$((limit_counter+1))
